720103d7d6023a93cd4f9c9597712ea22198464f
Julian Gruber <julian@juliangruber.com>
2020-02-24 09:46:49 +0100

    simplify using PROMPT_COMMAND

--- ps1.sh
+++ ps1.sh
@@ -2,5 +2,5 @@ function git_branch() {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \(\1\)/'
 }
 
-export PS1="\[\e[1;34m\]+ \[\e[0m\]\W\[\033[32m\]\$(git_branch)\[\033[00m\] : "
-#export PS1="\[\e[1;34m\] - \[\e[0m\] "
+export PS1="\[\e[1;34m\]$\[\e[0m\] "
+export PROMPT_COMMAND='echo -ne "\033]0;${PWD/#$HOME/~} $(git_branch)\007"'
