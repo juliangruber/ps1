89a18e97e37ec35d21546dab1a8b38a9278e97c4
Julian Gruber <julian@juliangruber.com>
2020-04-27 14:16:27 +0200

    fix prompt_command

--- ps1.zsh
+++ ps1.zsh
@@ -2,7 +2,7 @@ function git_branch() {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \(\1\)/'
 }
 
-export PS1='%F{12}$ %f'
+export PROMPT='%F{12}$ %f'
 
-export PROMPT_COMMAND='echo -ne "$(basename "$(dirname $PWD)")/$(basename $PWD) $(git_branch)"'
+export PROMPT_COMMAND='echo -ne "\033]0;$(basename "$(dirname $PWD)")/$(basename $PWD) $(git_branch)\007"'
 precmd() { eval "$PROMPT_COMMAND" }
